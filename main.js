465567e8c3d171eb189cdaee8913a7652b77ee79
Felicitatem <kashyapsamriddhi4@gmail.com>
2023-09-20 14:41:28 +0530

    Responsive website

--- main.js
+++ main.js
@@ -22,3 +22,25 @@ faqs.forEach(faq => {
     })
 }
 )
+
+//show/hide menu
+
+const menu=document.querySelector(".nav__menu");
+const menuBtn=document.querySelector("#open-menu-btn");
+const closeBtn=document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', ()=>{
+    menu.style.display="flex";
+    closeBtn.style.display="inline-block";
+    menuBtn.style.display="none";
+})
+
+//closenav menu
+
+const closeNav=()=>{
+    menu.style.display="none";
+    closeBtn.style.display="none";
+    menuBtn.style.display="inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
